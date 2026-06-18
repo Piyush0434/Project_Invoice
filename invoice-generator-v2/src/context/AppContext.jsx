ae05d2f91db3f2dd1aa888adb285e7c3dd87adcf
Piyush0434 <piyushghatage3@gmail.com>
2026-06-18 23:03:48 +0530

    update CORS for EC2 deployment

--- invoice-generator-v2/src/context/AppContext.jsx
+++ invoice-generator-v2/src/context/AppContext.jsx
@@ -20,7 +20,7 @@ export const AppContextProvider = (props) => {
   const [invoiceTitle, setInvoiceTitle] = useState("Create Invoice");
   const [selectedTemplate, setSelectedTemplate] = useState("template1");
 
-  const baseURL = "http://localhost:8080/api";
+  const baseURL = import.meta.env.VITE_API_BASE_URL||"http://localhost:8080/api";
 
   const contextValue = {
     baseURL,
